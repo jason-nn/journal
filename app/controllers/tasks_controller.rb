df1cefc8b071b972ca5beaeea8bf592371040ffd
jason-nn <jason.ho@obf.ateneo.edu>
2021-11-29 23:09:12 +0800

    added destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,14 @@
 class TasksController < ApplicationController
     def create
         @category = Category.find(params[:category_id])
-        @comment = @category.tasks.create(task_params)
+        @task = @category.tasks.create(task_params)
+        redirect_to category_path(@category)
+    end
+
+    def destroy
+        @category = Category.find(params[:category_id])
+        @task = @category.tasks.find(params[:id])
+        @task.destroy
         redirect_to category_path(@category)
     end
 
