649c44c4c75837fe65090a849f5ce3d381cab252
jason-nn <jason.ho@obf.ateneo.edu>
2021-12-01 21:03:37 +0800

    Model Spec: Category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -9,4 +9,16 @@ RSpec.describe 'Model Spec: Category', type: :model do
     it 'should not catch fire when you create an instance' do
         expect(@category).not_to eq nil
     end
+
+    it 'should hold and return name' do
+        expect(@category.name).to eq @name
+    end
+
+    it 'should need a name to be valid' do
+        expect(Category.create(name: nil)).not_to be_valid
+    end
+
+    it 'should have a unique name' do
+        expect(Category.create(name: @name)).not_to be_valid
+    end
 end
