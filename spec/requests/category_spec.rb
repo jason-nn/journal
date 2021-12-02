ea8456b99af1d330b3524da05d50fe1d2f4a7c6f
Jason Ho <jason.ho@obf.ateneo.edu>
2021-12-02 20:39:44 +0800

    Request Spec: Category

--- spec/requests/category_spec.rb
+++ spec/requests/category_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'Request Spec: Category', type: :request do
-    # describe 'GET /index' do
-    #     pending "add some examples (or delete) #{__FILE__}"
-    # end
+    it 'GET /categories' do
+      get '/categories'
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:index)
+    end
+
+    it 'GET /categories/new' do
+      get '/categories/new'
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:new)
+    end
+    
+    it 'GET /categories/:id/edit' do
+      category = Category.create(name: 'Test')
+      get edit_category_path(category)
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:edit)
+    end
+    
+    it 'GET /categories/:id' do
+      category = Category.create(name: 'Test')
+      get category_path(category)
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:show)
+    end
 end
