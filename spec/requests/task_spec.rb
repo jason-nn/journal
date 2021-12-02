ffc1989cecbc4a96163b9cdeab921e4200687513
Jason Ho <jason.ho@obf.ateneo.edu>
2021-12-02 20:53:49 +0800

    Request Spec: Task

--- spec/requests/task_spec.rb
+++ spec/requests/task_spec.rb
@@ -1,7 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Request Spec: Task', type: :request do
-    # describe 'GET /index' do
-    #     pending "add some examples (or delete) #{__FILE__}"
-    # end
+  it 'GET /' do
+    get '/'
+    expect(response).to have_http_status(200)
+    expect(response).to render_template(:today)
+  end
+
+  it 'GET /tasks' do
+    get '/tasks'
+    expect(response).to have_http_status(200)
+    expect(response).to render_template(:all)
+  end
+
+  it 'GET /categories/:category_id/tasks/:id/edit' do
+    task = Category.create(name: 'Test')
+    .tasks.create(
+      name: 'Task', 
+      details: 'task details', 
+      date: Date.today
+    )
+    get edit_category_task_path(task.category, task)
+    expect(response).to have_http_status(200)
+    expect(response).to render_template(:edit)
+  end
+
+  it 'GET /categories/:category_id/tasks/:id' do
+    task = Category.create(name: 'Test')
+    .tasks.create(
+      name: 'Task', 
+      details: 'task details', 
+      date: Date.today
+    )
+    get category_task_path(task.category, task)
+    expect(response).to have_http_status(200)
+    expect(response).to render_template(:show)
+  end
 end
