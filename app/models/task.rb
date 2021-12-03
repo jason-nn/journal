58b2658d910a8916469f71631158f98ea735c78f
Jason Ho <jason.ho@obf.ateneo.edu>
2021-12-04 02:04:05 +0800

    tabs

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
-    belongs_to :category
+  belongs_to :category
 
-    validates :name, presence: true
-    validates :details, presence: true
-    validates :date, presence: true
+  validates :name, presence: true
+  validates :details, presence: true
+  validates :date, presence: true
 end
