b00b4e8919a8170a917f4ca9f5640dbc963baccc
Jason Ho <jason.ho@obf.ateneo.edu>
2021-12-04 11:07:29 +0800

    validates presence of user id in categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,5 @@ class Category < ApplicationRecord
   has_many :tasks, dependent: :destroy
 
   validates :name, presence: true, uniqueness: {scope: [:user_id]}
+  validates :user_id, presence: true
 end
