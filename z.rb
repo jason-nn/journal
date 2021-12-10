62534aab73107b73fc2c3f8dac666621559f4d2a
Jason Ho <jason.ho@obf.ateneo.edu>
2021-12-10 19:40:31 +0800

    added exercise

--- z.rb
+++ z.rb
@@ -1,1 +1,16 @@
-puts 'a'
+def sum(arr)
+  return [] if !arr.respond_to? :each || arr.length < 1
+  output = [0, 0]
+  arr.each { |x| x > 0 ? output[0] += 1 : output[1] += x }
+  return output
+end
+
+sample = (1..15).map { |x| x < 11 ? x : -x }
+
+pp sample
+
+pp sum sample
+
+pp sum []
+
+pp sum nil
